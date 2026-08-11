e761a2dd9f18f59150bff79d1f868b0d400a378c
Aarav Malani <aarav.malani@gmail.com>
2026-08-11 23:17:01 +0800

    chore: replace Duke with ArthurDent

---
+++ src/main/java/ArthurDent.java
@@ -0,0 +1,11 @@
+public class ArthurDent {
+    public static void main(String[] args) {
+        // AI-assisted: Replaced the default Duke banner with ASCII art for "ARTHUR".
+        String banner = "    _    ____ _____ _   _ _   _ ____\n"
+                + "   / \\  |  _ \\_   _| | | | | | |  _ \\\n"
+                + "  / _ \\ | |_) || | | |_| | | | | |_) |\n"
+                + " / ___ \\|  _ < | | |  _  | |_| |  _ <\n"
+                + "/_/   \\_\\_| \\_\\|_| |_| |_|\\___/|_| \\_\\\n";
+        System.out.println(banner);
+    }
+}
